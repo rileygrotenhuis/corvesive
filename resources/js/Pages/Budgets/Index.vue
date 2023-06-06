c9adb939bdee6f98b60633253b5e9561869bd2e9
Riley Grotenhuis <rg@buildmidwestern.com>
2023-06-05 23:34:21 -0500

    chore: remove unused columns

--- resources/js/Pages/Budgets/Index.vue
+++ resources/js/Pages/Budgets/Index.vue
@@ -16,22 +16,17 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
                     <th class="border bg-slate-300 px-4 py-2">ID</th>
                     <th class="border bg-slate-300 px-4 py-2">Name</th>
                     <th class="border bg-slate-300 px-4 py-2">Amount</th>
-                    <th class="border bg-slate-300 px-4 py-2">Actions</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="budget in budgets" :key="budget.id">
-                    <td class="border px-4 py-2 text-center">{{ budget.id }}</td>
-                    <td class="border px-4 py-2 text-center">{{ budget.name }}</td>
-                    <td class="border px-4 py-2 text-center">${{ (budget.amount / 100).toFixed(2) }}</td>
-                    <td class="border px-4 py-2 flex gap-2 justify-center">
+                    <td class="border px-4 py-2 text-center">
                         <a :href="route('budgets.show', budget.id)">
-                            <PrimaryButton>Show</PrimaryButton>
-                        </a>
-                        <a :href="route('budgets.edit', budget.id)">
-                            <PrimaryButton>Edit</PrimaryButton>
+                            {{ budget.id }}
                         </a>
                     </td>
+                    <td class="border px-4 py-2 text-center">{{ budget.name }}</td>
+                    <td class="border px-4 py-2 text-center">${{ (budget.amount / 100).toFixed(2) }}</td>
                 </tr>
             </tbody>
         </table>
