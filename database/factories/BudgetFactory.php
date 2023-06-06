03a40f25e56ff7ea2b4ee9eb1785dc55a93401d6
Riley Grotenhuis <rg@buildmidwestern.com>
2023-06-05 21:50:03 -0500

    feat: add budget classes

---
+++ database/factories/BudgetFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Budget>
+ */
+class BudgetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'amount' => $this->faker->numberBetween(500, 10000),
+        ];
+    }
+}
