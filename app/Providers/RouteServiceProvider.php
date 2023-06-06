2067cac0a94bcb353b790438911fd519c862b871
Riley Grotenhuis <rg@buildmidwestern.com>
2023-06-06 14:30:23 -0500

    fix: add correct redirect

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/budgets';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
