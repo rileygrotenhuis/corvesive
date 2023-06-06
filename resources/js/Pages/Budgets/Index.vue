6fbf36e4f450be3fb7319817d5a64eb7844de637
Riley Grotenhuis <rg@buildmidwestern.com>
2023-06-05 22:56:48 -0500

    feat: display all budgets in clean table

--- resources/js/Pages/Budgets/Index.vue
+++ resources/js/Pages/Budgets/Index.vue
@@ -1,5 +1,6 @@
 <script setup>
-    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import PrimaryButton from '@/Components/PrimaryButton.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 
     defineProps({
         budgets: Array
@@ -9,8 +10,27 @@
 <template>
     <AuthenticatedLayout>
         <h1>All Budgets</h1>
-        <ul>
-            <li v-for="budget in budgets" :key="budget.id">{{ budget.name }}</li>
-        </ul>
+        <table class="w-full">
+            <thead>
+                <tr class="px-4 py-2">
+                    <th class="border bg-slate-300 px-4 py-2">ID</th>
+                    <th class="border bg-slate-300 px-4 py-2">Name</th>
+                    <th class="border bg-slate-300 px-4 py-2">Amount</th>
+                    <th class="border bg-slate-300 px-4 py-2">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="budget in budgets" :key="budget.id">
+                    <td class="border px-4 py-2 text-center">{{ budget.id }}</td>
+                    <td class="border px-4 py-2 text-center">{{ budget.name }}</td>
+                    <td class="border px-4 py-2 text-center">${{ (budget.amount / 100).toFixed(2) }}</td>
+                    <td class="border px-4 py-2 text-center">
+                        <a :href="route('budgets.edit', budget.id)">
+                            <PrimaryButton>Edit</PrimaryButton>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </AuthenticatedLayout>
 </template>
