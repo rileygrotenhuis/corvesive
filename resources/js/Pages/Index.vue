9b2e5d921fa38061795fc171d05b93e940249f37
Riley Grotenhuis <rg@buildmidwestern.com>
2023-06-21 22:22:48 -0500

    feat: add link to budgets page

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -10,7 +10,7 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Welcome" />
+    <Head title="Corvesive" />
 
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
@@ -18,9 +18,9 @@ defineProps({
         <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
             <Link
                 v-if="$page.props.auth.user"
-                :href="route('home')"
+                :href="route('budgets.index')"
                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >Dashboard</Link
+                >Budgets</Link
             >
 
             <template v-else>
