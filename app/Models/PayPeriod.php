445994aafd09823d010b9e4b91020d7238e31d39
Riley Grotenhuis <rg@buildmidwestern.com>
2023-07-08 03:52:08 -0500

    feat: setup relationships between expenses and pay periods

--- app/Models/PayPeriod.php
+++ app/Models/PayPeriod.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class PayPeriod extends Model
 {
@@ -13,5 +14,20 @@ class PayPeriod extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function budgets(): BelongsToMany
+    {
+        return $this->belongsToMany(Budget::class);
+    }
+
+    public function bills(): BelongsToMany
+    {
+        return $this->belongsToMany(Bill::class);
+    }
+
+    public function savings(): BelongsToMany
+    {
+        return $this->belongsToMany(Saving::class);
     }
 }
--- app/Models/Saving.php
+++ app/Models/Saving.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Saving extends Model
 {
@@ -13,5 +14,10 @@ class Saving extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function payPeriods(): BelongsToMany
+    {
+        return $this->belongsToMany(PayPeriod::class);
     }
 }
