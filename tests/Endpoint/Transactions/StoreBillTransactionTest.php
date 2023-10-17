14d809c46c2f6952a36b0a71ae664507351e6ec0
Riley Grotenhuis <rg@buildmidwestern.com>
2023-10-16 21:31:08 -0500

    feat: skeleton out classes for transaction endpoint testing

---
+++ tests/Endpoint/Transactions/StoreBillTransactionTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Endpoint\Transactions;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class StoreBillTransactionTest extends TestCase
+{
+    use RefreshDatabase;
+}
---
+++ tests/Endpoint/Transactions/StoreBudgetTransactionTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Endpoint\Transactions;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class StoreBudgetTransactionTest extends TestCase
+{
+    use RefreshDatabase;
+}
