6e4d04b7d523c4899ade0e92889894e8f7fd9a09
Riley Grotenhuis <rg@buildmidwestern.com>
2023-10-17 18:22:08 -0500

    feat: add skeleton parent setup classes to tests

--- tests/Endpoint/Transactions/StoreBillTransactionTest.php
+++ tests/Endpoint/Transactions/StoreBillTransactionTest.php
@@ -2,10 +2,22 @@
 
 namespace Tests\Endpoint\Transactions;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class StoreBillTransactionTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected User $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+
+        $this->authenticatesUser($this->user);
+    }
 }
