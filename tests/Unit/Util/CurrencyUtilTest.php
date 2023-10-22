d9b49b284f9499c97adfa2c9978124acf0e36a9a
Riley Grotenhuis <rg@buildmidwestern.com>
2023-10-22 18:22:20 -0500

    feat: write unit tests for currency util class

---
+++ tests/Unit/Util/CurrencyUtilTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit\Util;
+
+use App\Util\CurrencyUtil;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CurrencyUtilTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_format_currency_values_with_whole_number(): void
+    {
+        $amount = 10000;
+        $result = CurrencyUtil::formatCurrencyValues($amount);
+
+        $this->assertEquals(collect([
+            'raw' => 10000,
+            'pretty' => '$100.00',
+        ]), $result);
+    }
+
+    public function test_format_currency_values_with_even_cents(): void
+    {
+        $amount = 12550;
+        $result = CurrencyUtil::formatCurrencyValues($amount);
+
+        $this->assertEquals(collect([
+            'raw' => 12550,
+            'pretty' => '$125.50',
+        ]), $result);
+    }
+
+    public function test_format_currency_values_with_odd_cents(): void
+    {
+        $amount = 12567;
+        $result = CurrencyUtil::formatCurrencyValues($amount);
+
+        $this->assertEquals(collect([
+            'raw' => 12567,
+            'pretty' => '$125.67',
+        ]), $result);
+    }
+}
