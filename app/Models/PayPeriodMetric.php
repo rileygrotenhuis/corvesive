56cb98ccdc2179609c18c9241ac374c1449c9ce1
Riley Grotenhuis <rg@buildmidwestern.com>
2023-10-24 20:39:20 -0500

    feat: create metrics table

---
+++ app/Models/PayPeriodMetric.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PayPeriodMetric extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'pay_period_metrics';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function payPeriod(): BelongsTo
+    {
+        return $this->belongsTo(PayPeriod::class);
+    }
+}
