e7854644ed4cbc4d951130be5bacdf75f2087ce2
Riley Grotenhuis <rg@buildmidwestern.com>
2023-10-28 12:44:01 -0500

    feat: use pivot value on repository for metrics

--- app/Repositories/PayPeriodMetricsRepository.php
+++ app/Repositories/PayPeriodMetricsRepository.php
@@ -14,10 +14,12 @@ class PayPeriodMetricsRepository
     public function getBillMetrics(): Collection
     {
         $totalPayed = $this->payPeriod->bills()
+            ->whereNull('pay_period_bill.deleted_at')
             ->where('pay_period_bill.has_payed', 1)
             ->sum('pay_period_bill.amount');
 
         $totalUnpayed = $this->payPeriod->bills()
+            ->whereNull('pay_period_bill.deleted_at')
             ->where('pay_period_bill.has_payed', 0)
             ->sum('pay_period_bill.amount');
 
@@ -33,9 +35,11 @@ class PayPeriodMetricsRepository
         return collect([
             'total_balance' => $this->payPeriod
                 ->budgets()
+                ->whereNull('pay_period_budget.deleted_at')
                 ->sum('pay_period_budget.total_balance'),
             'remaining_balance' => $this->payPeriod
                 ->budgets()
+                ->whereNull('pay_period_budget.deleted_at')
                 ->sum('pay_period_budget.remaining_balance'),
         ]);
     }
@@ -43,19 +47,24 @@ class PayPeriodMetricsRepository
     public function getIncomeMetrics(): Collection
     {
         return collect([
-            'paystubs_total' => $this->payPeriod->paystubs->sum('amount'),
+            'paystubs_total' => $this->payPeriod
+                ->paystubs()
+                ->whereNull('pay_period_paystub.deleted_at')
+                ->sum('pay_period_paystub.amount'),
         ]);
     }
 
     public function getTransactionMetrics(): Collection
     {
         $billSpent = $this->payPeriod->transactions()
+            ->whereNull('transactions.deleted_at')
             ->whereNull('pay_period_budget_id')
             ->whereNotNull('pay_period_bill_id')
             ->where('type', '=', 'payment')
             ->sum('amount');
 
         $budgetSpent = $this->payPeriod->transactions()
+            ->whereNull('transactions.deleted_at')
             ->whereNotNull('pay_period_budget_id')
             ->whereNull('pay_period_bill_id')
             ->where('type', '=', 'payment')
@@ -68,6 +77,7 @@ class PayPeriodMetricsRepository
                 'total' => $budgetSpent + $billSpent,
             ],
             'deposit' => $this->payPeriod->transactions()
+                ->whereNull('transactions.deleted_at')
                 ->whereNull('pay_period_budget_id')
                 ->whereNull('pay_period_bill_id')
                 ->where('type', '=', 'deposit')
