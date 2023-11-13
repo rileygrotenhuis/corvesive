7b10116d55574371fa9404702e31b675bac79bcc
Riley Grotenhuis <rg@buildmidwestern.com>
2023-11-13 10:19:42 -0600

    wip

--- v2.corvesive.com/middleware/auth.ts
+++ v2.corvesive.com/middleware/auth.ts
@@ -1,15 +1,22 @@
 import type { IUserResource } from '~/http/resources/account.resource';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  if (Object.keys(useAccountStore().user).length > 0) {
-    return;
+  const accountStore = useAccountStore();
+  const payPeriodStore = usePayPeriodStore();
+
+  if (Object.keys(accountStore.user).length < 1 || !useCookie('corvesive_access_token').value) {
+    const me: IUserResource = await accountStore.me();
+
+    if (!me) {
+      return await navigateTo('/login');
+    }
+
+    if (payPeriodStore.payPeriods.length < 1) {
+      await payPeriodStore.getPayPeriods();
+    }
+
+    if (Object.keys(payPeriodStore.currentPayPeriod).length < 1) {
+      await payPeriodStore.getPayPeriod(me.pay_period.id);
+    }
   }
-
-  const me: IUserResource = await useAccountStore().me();
-
-  if (!me) {
-    return await navigateTo('/login');
-  }
-
-  await usePayPeriodStore().getPayPeriod(me.pay_period.id);
 });
