96d4ff0c11184ef2895027743995d341f6850b18
Riley Grotenhuis <rg@buildmidwestern.com>
2023-11-15 00:37:33 -0600

    feat: create expense card

---
+++ v2.corvesive.com/components/expenses/ExpenseCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps({
+  title: String,
+  subtitle: String,
+  amount: String,
+  amountSubtitle: String,
+});
+</script>
+
+<template>
+  <div class="hover:cursor-pointer shadow-sm hover:shadow-2xl">
+    <UCard>
+      <div class="flex flex-row justify-between items-center">
+        <div>
+          <p class="font-bold">{{ title }}</p>
+          <p v-if="subtitle" class="font-light text-sm">{{ subtitle }}</p>
+        </div>
+        <div>
+          <p>{{ amount }}</p>
+          <p v-if="amountSubtitle" class="text-xs text-rose-500 font-bold">
+            {{ amountSubtitle }}
+          </p>
+        </div>
+      </div>
+    </UCard>
+  </div>
+</template>
