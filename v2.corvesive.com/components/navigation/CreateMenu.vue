775ff22aa08aedee625c5ae5ed210af9c64ddb74
Riley Grotenhuis <rg@buildmidwestern.com>
2023-11-16 01:10:43 -0600

    wip

--- v2.corvesive.com/components/navigation/CreateMenu.vue
+++ v2.corvesive.com/components/navigation/CreateMenu.vue
@@ -5,25 +5,32 @@ const modalStore = useModalStore();
 const menuItems = [
   [
     {
-      label: 'New Paystub',
+      label: 'Create Recurring:',
+      slot: 'title',
+      disabled: true,
+    },
+  ],
+  [
+    {
+      label: '+ Paystub',
       click: () => {
         modalStore.openRecurringModal('paystubs');
       },
     },
     {
-      label: 'New Bill',
+      label: '+ Bill',
       click: () => {
         modalStore.openRecurringModal('bills');
       },
     },
     {
-      label: 'New Budget',
+      label: '+ Budget',
       click: () => {
         modalStore.openRecurringModal('budgets');
       },
     },
     {
-      label: 'New Saving',
+      label: '+ Saving',
       click: () => {
         modalStore.openRecurringModal('savings');
       },
@@ -43,5 +50,22 @@ const menuItems = [
       :alt="accountStore.user.names.full"
       class="w-5 h-5"
     />
+
+    <template #title="{ item }">
+      <div class="text-left">
+        <p class="truncate font-medium text-gray-900 dark:text-white">
+          {{ item.label }}
+        </p>
+      </div>
+    </template>
+
+    <template #item="{ item }">
+      <span class="truncate">{{ item.label }}</span>
+
+      <UIcon
+        :name="item.icon"
+        class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto"
+      />
+    </template>
   </UDropdown>
 </template>
--- v2.corvesive.com/components/recurring/CreateRecurringModal.vue
+++ v2.corvesive.com/components/recurring/CreateRecurringModal.vue
@@ -9,13 +9,11 @@ const modalStore = useModalStore();
     :ui="{ container: 'flex items-start items-top justify-center text-right' }"
   >
     <div class="p-4">
-      <div class="flex justify-end">
-        <UIcon
-          name="i-heroicons-x-mark"
-          class="w-8 h-8 hover:cursor-pointer"
-          @click="modalStore.closeRecurringModal()"
-        />
-      </div>
+      <UIcon
+        name="i-heroicons-x-mark"
+        class="w-8 h-8 hover:cursor-pointer absolute top-5 right-3"
+        @click="modalStore.closeRecurringModal()"
+      />
       <RecurringCreatePaystubForm
         v-if="modalStore.recurring.type === 'paystubs'"
       />
