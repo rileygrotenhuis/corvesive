a4ad025b8090632a215e1633e1e1ccd9718c1987
Riley Grotenhuis <rg@buildmidwestern.com>
2024-02-18 20:24:13 -0600

    fix: add max age for cookie

--- www.corvesive.com/middleware/auth.ts
+++ www.corvesive.com/middleware/auth.ts
@@ -3,7 +3,10 @@ import type { IUserResource } from '~/http/resources/account.resource';
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const accountStore = useAccountStore();
   const payPeriodStore = usePayPeriodStore();
-  const accessToken = useCookie('corvesive_access_token').value;
+  const accessToken = useCookie('corvesive_access_token', {
+    maxAge: 60 * 60 * 24 * 7,
+    path: '/',
+  }).value;
 
   const isUserLoggedIn =
     Object.keys(accountStore.user).length > 0 && accessToken;
--- www.corvesive.com/util/auth.util.ts
+++ www.corvesive.com/util/auth.util.ts
@@ -1,6 +1,9 @@
 export async function setAccessToken(token: string): Promise<string> {
   return new Promise((resolve) => {
-    useCookie('corvesive_access_token').value = token;
+    useCookie('corvesive_access_token', {
+      maxAge: 60 * 60 * 24 * 7,
+      path: '/',
+    }).value = token;
     resolve(token);
   });
 }
