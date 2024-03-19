e2c78aa2cb5a84ab411f5d12560a225b1ea5eff5
Riley Grotenhuis <rg@buildmidwestern.com>
2024-03-19 17:09:12 -0500

    feat: add pay period to manager class constructors

--- app/Services/PayPeriodBillManager.php
+++ app/Services/PayPeriodBillManager.php
@@ -2,11 +2,14 @@
 
 namespace App\Services;
 
+use App\Models\PayPeriod;
 use App\Models\User;
 
 class PayPeriodBillManager
 {
-    public function __construct(protected User $user)
-    {
+    public function __construct(
+        protected User $user,
+        protected PayPeriod $payPeriod
+    ) {
     }
 }
