ba7577c241bc4b89206279de8ca597435b7e5c61
Riley Grotenhuis <rg@buildmidwestern.com>
2024-03-22 23:10:25 -0500

    fix: unauth users

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -32,7 +32,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => $request->user()->load('currentPayPeriod'),
+                'user' => $request->user() ? $request->user()->load('currentPayPeriod') : null,
                 'payPeriods' => $request->user() ? $request->user()->payPeriods()->latest()->get() : [],
             ],
         ];
