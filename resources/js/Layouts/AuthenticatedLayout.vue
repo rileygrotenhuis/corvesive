3aa0bcc06117512b30a1ab8a251ed8d8341ecbb1
Riley Grotenhuis <rg@buildmidwestern.com>
2024-03-27 21:29:07 -0500

    navbar fixes

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,63 +1,55 @@
 <script setup>
-import { ref } from 'vue';
+import { Link, usePage } from '@inertiajs/vue3';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import NavLink from '@/Components/NavLink.vue';
+import { ref } from 'vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link, usePage } from '@inertiajs/vue3';
 
 const showingNavigationDropdown = ref(false);
 
 const userHasPayPeriod = Boolean(usePage().props.auth.user.current_pay_period);
+
+const navigationLinks = [
+  {
+    href: userHasPayPeriod ? 'pay-periods.index' : 'pay-periods.create',
+    label: 'Pay Periods',
+  },
+  {
+    href: 'income.index',
+    label: 'Income',
+  },
+  {
+    href: 'expenses.index',
+    label: 'Expenses',
+  },
+];
 </script>
 
 <template>
-  <div>
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-      <nav
-        class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700"
-      >
-        <!-- Primary Navigation Menu -->
+  <div class="bg-zinc-900 text-white">
+    <div class="min-h-screen">
+      <nav class="bg-black border-b border-gray-700">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="flex justify-between h-16">
             <div class="flex">
-              <!-- Logo -->
               <div class="shrink-0 flex items-center">
                 <Link :href="route('dashboard')">
                   <ApplicationLogo
-                    class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
+                    class="block h-9 w-auto fill-current text-gray-200"
                   />
                 </Link>
               </div>
 
-              <!-- Navigation Links -->
               <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                 <NavLink
-                  :href="
-                    userHasPayPeriod
-                      ? route('pay-periods.index')
-                      : route('pay-periods.create')
-                  "
-                  :active="
-                    userHasPayPeriod
-                      ? route().current('pay-periods.index')
-                      : route().current('pay-periods.create')
-                  "
+                  v-for="(nav, index) in navigationLinks"
+                  :key="index"
+                  :href="route(nav.href)"
+                  :active="route().current(nav.href)"
                 >
-                  Pay Periods
-                </NavLink>
-                <NavLink
-                  :href="route('income.index')"
-                  :active="route().current('income.index')"
-                >
-                  Income
-                </NavLink>
-                <NavLink
-                  :href="route('expenses.index')"
-                  :active="route().current('expenses.index')"
-                >
-                  Expenses
+                  {{ nav.label }}
                 </NavLink>
               </div>
             </div>
@@ -66,26 +58,23 @@ const userHasPayPeriod = Boolean(usePage().props.auth.user.current_pay_period);
               <div class="space-x-8 sm:-my-px sm:ms-10 sm:flex">
                 <NavLink
                   v-if="userHasPayPeriod"
-                  gi
                   :href="route('transactions.create')"
                   :active="route().current('transactions.create')"
                 >
-                  Transaction
+                  + Transaction
                 </NavLink>
               </div>
 
               <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <!-- Settings Dropdown -->
                 <div class="ms-3 relative">
                   <Dropdown align="right" width="48">
                     <template #trigger>
                       <span class="inline-flex rounded-md">
                         <button
                           type="button"
-                          class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                          class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white focus:outline-none transition ease-in-out duration-150"
                         >
                           {{ $page.props.auth.user.name }}
-
                           <svg
                             class="ms-2 -me-0.5 h-4 w-4"
                             xmlns="http://www.w3.org/2000/svg"
@@ -119,11 +108,10 @@ const userHasPayPeriod = Boolean(usePage().props.auth.user.current_pay_period);
               </div>
             </div>
 
-            <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
               <button
                 @click="showingNavigationDropdown = !showingNavigationDropdown"
-                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
+                class="inline-flex items-center justify-center p-2 rounded-md text-gray-500 transition duration-150 ease-in-out"
               >
                 <svg
                   class="h-6 w-6"
@@ -157,7 +145,6 @@ const userHasPayPeriod = Boolean(usePage().props.auth.user.current_pay_period);
           </div>
         </div>
 
-        <!-- Responsive Navigation Menu -->
         <div
           :class="{
             block: showingNavigationDropdown,
@@ -167,42 +154,21 @@ const userHasPayPeriod = Boolean(usePage().props.auth.user.current_pay_period);
         >
           <div class="pt-2 pb-3 space-y-1">
             <ResponsiveNavLink
-              :href="
-                userHasPayPeriod
-                  ? route('pay-periods.index')
-                  : route('pay-periods.create')
-              "
-              :active="
-                userHasPayPeriod
-                  ? route().current('pay-periods.index')
-                  : route().current('pay-periods.create')
-              "
+              v-for="(nav, index) in navigationLinks"
+              :key="index"
+              :href="route(nav.href)"
+              :active="route().current(nav.href)"
             >
-              Pay Periods
-            </ResponsiveNavLink>
-            <ResponsiveNavLink
-              :href="route('income.index')"
-              :active="route().current('income.index')"
-            >
-              Income
-            </ResponsiveNavLink>
-            <ResponsiveNavLink
-              :href="route('expenses.index')"
-              :active="route().current('expenses.index')"
-            >
-              Expenses
+              {{ nav.label }}
             </ResponsiveNavLink>
           </div>
 
-          <!-- Responsive Settings Options -->
-          <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+          <div class="pt-4 pb-1 border-t border-gray-600">
             <div class="px-4">
-              <div
-                class="font-medium text-base text-gray-800 dark:text-gray-200"
-              >
+              <div class="font-medium text-base text-gray-200">
                 {{ $page.props.auth.user.name }}
               </div>
-              <div class="font-medium text-sm text-gray-500">
+              <div class="font-medium text-gray-500">
                 {{ $page.props.auth.user.email }}
               </div>
             </div>
@@ -223,17 +189,21 @@ const userHasPayPeriod = Boolean(usePage().props.auth.user.current_pay_period);
         </div>
       </nav>
 
-      <!-- Page Heading -->
-      <header class="bg-white dark:bg-gray-800 shadow" v-if="$slots.header">
+      <header class="bg-black shadow" v-if="$slots.header">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <slot name="header" />
         </div>
       </header>
 
-      <!-- Page Content -->
       <main>
         <slot />
       </main>
     </div>
   </div>
 </template>
+
+<style scoped>
+.main-container {
+  background-color: white !important;
+}
+</style>
