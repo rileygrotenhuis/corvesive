b694c04ca9029a496606932921b52a94de863d69
Riley Grotenhuis <rg@buildmidwestern.com>
2024-04-04 12:39:46 -0500

    fix: remove dashboard page

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -35,7 +35,7 @@ const navigationLinks = [
           <div class="flex justify-between h-16">
             <div class="flex">
               <div class="shrink-0 flex items-center">
-                <Link :href="route('dashboard')">
+                <Link :href="route('pay-periods.index')">
                   <ApplicationLogo
                     class="block h-9 w-auto fill-current text-gray-200"
                   />
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DepositController;
 use App\Http\Controllers\ExpensesController;
 use App\Http\Controllers\IncomeController;
@@ -33,8 +32,6 @@ Route::get('/playground', function () {
 })->name('playground');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', DashboardController::class)->name('dashboard');
-
     Route::prefix('income')->group(function () {
         Route::get('/', IncomeController::class)->name('income.index');
 
