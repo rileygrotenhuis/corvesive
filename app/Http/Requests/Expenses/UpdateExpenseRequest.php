95b3ca6189a0dd0c6a69eaf736c05c4a06546227
Riley Grotenhuis <rg@buildmidwestern.com>
2024-05-25 09:33:35 -0500

    move files

--- app/Http/Requests/Expenses/UpdateExpenseRequest.php
+++ app/Http/Requests/Expenses/UpdateExpenseRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Expenses;
 
 class UpdateExpenseRequest extends StoreExpenseRequest
 {
--- app/Http/Requests/Paystubs/StorePaystubRequest.php
+++ app/Http/Requests/Paystubs/StorePaystubRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Paystubs;
 
 use App\Traits\PaystubRecurrence;
 use Illuminate\Foundation\Http\FormRequest;
