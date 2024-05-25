ac50182516db3404301208fad6a8e62a75ab8a51
Riley Grotenhuis <rg@buildmidwestern.com>
2024-05-24 22:40:00 -0500

    rename components

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Navbar from '@/Layouts/Partials/Navbar.vue';
+import GuestNavbar from '@/Layouts/Partials/GuestNavbar.vue';
 import Footer from '@/Layouts/Partials/Footer.vue';
 </script>
 
 <template>
   <div>
-    <Navbar />
+    <GuestNavbar />
 
     <main>
       <slot />
