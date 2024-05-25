78c70aea7587112cdc9de1ed93a850e204a59afb
Riley Grotenhuis <rg@buildmidwestern.com>
2024-05-24 22:10:21 -0500

    registration and login left pane

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,5 +1,13 @@
+<script setup>
+import AuthBannerPane from '@/Pages/Auth/Partials/AuthBannerPane.vue';
+</script>
+
 <template>
-  <div>
-    <h1>Login</h1>
+  <div class="h-[100vh] flex">
+    <AuthBannerPane class="w-1/2" />
+
+    <div class="w-1/2 bg-white">
+      <h1>Login</h1>
+    </div>
   </div>
 </template>
---
+++ resources/js/Pages/Auth/Partials/AuthBannerPane.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AuthSketch from '@/Components/Sketches/AuthSketch.vue';
+</script>
+
+<template>
+  <div class="flex justify-center items-center flex-col">
+    <AuthSketch />
+    <div class="max-w-[350px] text-center mt-8">
+      <h4 class="text-white text-3xl font-semibold mb-2">Corvesive.</h4>
+      <p>
+        Budgeting done the way that you want and when you want it. Skipping all
+        the hassle..
+      </p>
+    </div>
+  </div>
+</template>
