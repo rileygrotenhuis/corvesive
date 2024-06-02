3f981524678b90280851e74024d3a5a4402be379
Riley Grotenhuis <rg@buildmidwestern.com>
2024-06-01 22:05:31 -0500

    wip

---
+++ resources/js/Pages/Dashboard/Partials/NewTransaction.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { ref } from 'vue';
+import { useForm } from '@inertiajs/vue3';
+import Modal from '@/Components/Breeze/Modal.vue';
+import InputError from '@/Components/Breeze/InputError.vue';
+
+const showModal = ref(false);
+
+const transactionForm = useForm({
+  date: new Date().toISOString().substr(0, 10),
+  amount: '',
+  amount_in_cents: '',
+  notes: '',
+});
+
+const submitTransactionForm = () => {
+  alert('submitting...');
+};
+</script>
+
+<template>
+  <button
+    @click.prevent="showModal = true"
+    class="w-8 h-8 flex text-center justify-center items-center bg-primary-100 p-2 text-primary-1000 font-bold rounded-full hover:bg-primary-700 hover:text-primary-100 transition ease-in-out"
+  >
+    +
+  </button>
+
+  <Modal :show="showModal" @close="showModal = false">
+    <form class="p-6 space-y-6" @submit.prevent="submitTransactionForm">
+      <h3 class="text-lg font-medium leading-6 text-gray-900">
+        New Transaction
+      </h3>
+      <div>
+        <label
+          for="deposit_date"
+          class="block text-sm font-medium text-gray-700"
+        >
+          Transaction Date
+        </label>
+        <input
+          v-model="transactionForm.date"
+          id="deposit_date"
+          name="deposit_date"
+          type="date"
+          class="mt-1 text-black block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+        />
+      </div>
+
+      <div>
+        <label for="amount" class="block text-sm font-medium text-gray-700">
+          Amount
+        </label>
+        <input
+          v-model="transactionForm.amount"
+          id="amount"
+          name="amount"
+          type="text"
+          class="mt-1 text-black w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+        />
+        <InputError :message="transactionForm.errors.amount_in_cents" />
+      </div>
+
+      <div>
+        <label for="notes" class="block text-sm font-medium text-gray-700">
+          Notes
+        </label>
+        <textarea
+          v-model="transactionForm.notes"
+          id="notes"
+          name="notes"
+          class="mt-1 text-black w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+          rows="4"
+        />
+        <InputError :message="transactionForm.errors.notes" />
+      </div>
+
+      <div class="flex justify-end">
+        <button
+          type="submit"
+          class="inline-flex items-center px-4 py-2 bg-primary-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary-800 focus:bg-primary-800 active:bg-primary-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+        >
+          Submit Deposit
+        </button>
+      </div>
+    </form>
+  </Modal>
+</template>
--- resources/js/Pages/Dashboard/Partials/Transactions.vue
+++ resources/js/Pages/Dashboard/Partials/Transactions.vue
@@ -1,6 +1,7 @@
 <script setup>
 import PaymentBanner from '@/Components/Transactions/PaymentBanner.vue';
 import DepositBanner from '@/Components/Transactions/DepositBanner.vue';
+import NewTransaction from "@/Pages/Dashboard/Partials/NewTransaction.vue";
 
 defineProps({
   transactions: Array,
@@ -9,7 +10,11 @@ defineProps({
 
 <template>
   <div class="max-w-3xl bg-white p-6 rounded-lg">
-    <h2 class="text-2xl font-bold text-primary-700">Transactions</h2>
+    <div class="flex justify-between items-center">
+      <h2 class="text-2xl font-bold text-primary-700">Transactions</h2>
+
+      <NewTransaction />
+    </div>
 
     <div class="space-y-4 mt-4">
       <component
