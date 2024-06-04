16f464a8ad9da25368c7322d06b871d134fcda33
Riley Grotenhuis <rg@buildmidwestern.com>
2024-06-03 22:38:04 -0500

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,35 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->adminUser();
+
+        /**
+         * Creates 1000 test users and generates paystubs
+         * and monthly expenses for each of those users.
+         */
+        $user = User::factory(1000)->create();
+        $user->each(function (User $user) {
+            $paystub = Paystub::factory()->for($user)->create([
+                'recurrence_rate' => 'monthly',
+                'recurrence_interval_one' => 1,
+            ]);
+
+            $paystub->generateFutureExpenses();
+
+            $expenses = Expense::factory(10)->for($user)->create();
+
+            $expenses->each(function (Expense $expense) {
+                $expense->generateFutureExpenses();
+            });
+        });
+    }
+
+    /**
+     * Seeds the database with an admin user
+     * that can be used for development.
+     */
+    protected function adminUser(): void
+    {
         /** Admin User */
         $user = User::factory()->create([
             'first_name' => 'Dev',
